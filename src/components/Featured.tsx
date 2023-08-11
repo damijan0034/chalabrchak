da6f8247c98db85be236eee3e823a7b441c18c8c
Goran Kostic <damijan0034@gmail.com>
2023-08-11 07:07:58 +0200

    Offer section

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -3,19 +3,22 @@ import { featuredProducts } from '@/data'
 
 export default function Featured() {
   return (
-    <div className='w-full overflow-x-scroll text-red-500'>
+    <div className='w-full overflow-x-scroll text-red-500 '>
         {/* WRAPPER */}
         <div className='w-max flex'>
             {/* SINGLE ITEM */}
             {featuredProducts.map(item=>(
               <div className='w-screen h-[60vh] flex flex-col justify-around
-            items-center p-4
+             p-2 hover:bg-fuchsia-50 transition-all duration-300 items-center
+             md:w-[50vw] lg:w-[33vw]
             ' key={item.id}>
              
                {/* IMAGE CONTAINER */}
                {
                 item.img &&
-                <div className='relative flex-1 w-full'>
+                <div className='relative flex-1 w-full
+                hover:rotate-[60deg] transition-all duration-500
+                '>
                 <Image src={item.img} alt={item.title} fill
                 className='object-contain'
                 />
@@ -23,9 +26,9 @@ export default function Featured() {
                }
                
                {/* TEXT CONTAINER */}
-               <div className='flex-1 flex flex-col gap-4'>
+               <div className='flex-1 flex flex-col gap-2 text-center  '>
                 <h1 className='text-xl font-bold uppercase '>{item.title}</h1>
-                <p>{item.desc}</p>
+                <p className=''>{item.desc}</p>
                 <span className='text-xl font-bold'>{item.price}</span>
                 <button className='bg-red-500 text-white rounded-md p-2'>
                   Add to cart
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Offer() {
   return (
-    <div>Offer</div>
+    <div className='bg-black h-screen md:flex-row flex flex-col'>
+      {/* TEXT CONTAINER */}
+      <div className='flex-1 flex flex-col justify-center
+      items-center text-center gap-8 p-6
+      '>
+        <h1 className='text-white text-5xl font-bold'>
+        Delicious Burger & French Fry
+        </h1>
+        <p className='text-white'>
+        Progressively simplify effective e-toilers and process-centric methods
+          of empowerment. Quickly pontificate parallel.
+        </p>
+        <button>Order Now</button>
+      </div>
+      {/* IMAGE CONTAINER */}
+      <div className='relative flex-1 w-full'>
+          <Image src="/temporary/offerProduct.png" alt=''
+          className='object-contain' fill />
+      </div>
+    </div>
   )
 }
